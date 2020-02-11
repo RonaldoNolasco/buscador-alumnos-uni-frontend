d6ce613f007d58d50732af8fa10a28e3ec8452cf
TheBestShooter <rnolascoc@uni.pe>
2020-02-11 00:25:49 -0500

    agregando bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
+    <div id="app">
+    <Navbar/>
     <div class="field is-grouped">
-      <input class="input is-info" type="text" placeholder="Info input" v-model="codigo" @keyup.enter="buscar">
-      <button class="button is-info is-expanded" @click="buscar">Buscar</button>
+        <input class="input is-info" type="text" placeholder="Info input" v-model="codigo" @keyup.enter="buscar">
+        <button class="button is-info is-expanded" @click="buscar">Buscar</button>
     </div>
     <br>
     <table class="table center">
-      <thead  v-if="validos.length>0">
-        <tr>
-          <td><strong>Codigo</strong></td>
-          <td><strong>Especialidad</strong></td>
-          <td><strong>Nombre</strong></td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="valido in validos" v-bind:key="valido.codigo">
-          <td>{{valido.codigo}}</td>
-          <td>{{valido.especialidad}}</td>
-          <td>{{valido.nombre}}</td>
-        </tr>
-      </tbody>
+        <thead  v-if="validos.length>0">
+            <tr>
+                <td><strong>Codigo</strong></td>
+                <td><strong>Especialidad</strong></td>
+                <td><strong>Nombre</strong></td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="valido in validos" v-bind:key="valido.codigo">
+                <td>{{valido.codigo}}</td>
+                <td>{{valido.especialidad}}</td>
+                <td>{{valido.nombre}}</td>
+            </tr>   
+        </tbody>
     </table>
-    
   </div>
 </template>
 
@@ -29,6 +29,7 @@
 //se puede usar v-for con li y creo que no es necesario usar computadas
 //ver lo del require json
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app', 
@@ -40,7 +41,8 @@ export default {
     }
   },
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   },
 //for in es para objetos, for of es para arreglos        
   methods:{
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import store from './store'
 import './../node_modules/bulma/css/bulma.css';
 
+
 Vue.config.productionTip = false
 
 new Vue({
