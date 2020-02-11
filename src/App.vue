9f4a95e943f5c4d440022f7912bd43fb24855797
TheBestShooter <rnolascoc@uni.pe>
2020-02-11 02:59:49 -0500

    configurando los tipos de busqueda

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
     <div id="app">
         <Navbar/>
-        <form class="form input-group mb-3">
-            <input class="form-control mr-sm-2" type="text" placeholder="Ingrese el codigo" aria-label="Search" v-model="codigo" @keyup.enter="buscar">
-            <div class="input-group-append">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" @click="buscar">Buscar</button>
+        <div class="p-3 bg-dark">
+            <div class="input-group">
+                <input class="form-control mr-sm-2" type="text" placeholder="Ingrese el codigo" aria-label="Search" v-model="entrada" @keyup.enter="buscar">
+                <select class="mr-sm-2" v-model="tipo">
+                    <option value="codigo" selected="selected">Código</option>
+                    <option value="ap">Apellido Paterno</option>
+                    <option value="am">Apellido Materno</option>
+                    <option value="nombre">Nombre</option>
+                </select>
+                <button class="btn btn-outline-success my-2 my-sm-0" @click="buscar">Buscar</button> 
             </div>
-        </form>
-        <br>
-        <table class="table">
+        </div>
+        <table class="table tabla bg-dark text-white">
             <thead v-if="validos.length>0">
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">Codigo</th>
                     <th scope="col">Especialidad</th>
-                    <th scope="col">A. P.</th>
-                    <th scope="col">A. M.</th>
+                    <th scope="col">Apellido Paterno</th>
+                    <th scope="col">Apellido Materno</th>
                     <th scope="col">Nombres</th>
                 </tr>
             </thead>
@@ -39,46 +44,114 @@
 import Navbar from './components/Navbar.vue'
 
 export default {
-  name: 'app', 
-  data(){
-    return{
-      codigo:"",
-      datos:require('./../public/codigos.json'),
-      validos:[],
-    }
-  },
-  components: {
-    Navbar,
-  },
-//for in es para objetos, for of es para arreglos        
+    name: 'app', 
+    data(){
+        return{
+        entrada:"",
+        tipo:"codigo",
+        datos:require('./../public/codigos.json'),
+        validos:[],
+        validarElemento: true
+        }
+    },
+    components: {
+        Navbar,
+    },
+//for in es para objetos, for of es para arreglos
     methods:{
         buscar(){
-        this.validos=[]
-        if(this.codigo.length>5){
-            var cad = []
-            let valido=true
-            for (var dato of this.datos){
-                valido=true
-                var cad = dato.codigo
-            var test = cad.substr(0,this.codigo.length)
-            if(test!=this.codigo){
-                valido=false
-            }
-            if(valido){
-                this.validos.push(
-                    {
-                    "id": this.validos.length + 1,
-                    "codigo": dato.codigo,
-                    "especialidad": dato.especialidad,
-                    "ap": dato.nombre.split('-')[0],
-                    "am": dato.nombre.split('-')[1],
-                    "nombre": dato.nombre.split('-')[2]
+            this.validos=[]
+            if(this.tipo=="codigo"){
+                if(this.entrada.length>5){
+                    let valido=true
+                    for (var dato of this.datos){
+                        valido=true
+                        var comp = dato.codigo.substr(0,this.entrada.length)
+                        if(comp!=this.entrada) valido=false;
+                        if(valido){
+                            this.validos.push(
+                                {
+                                "id": this.validos.length + 1,
+                                "codigo": dato.codigo,
+                                "especialidad": dato.especialidad,
+                                "ap": dato.nombre.split('-')[0],
+                                "am": dato.nombre.split('-')[1],
+                                "nombre": dato.nombre.split('-')[2]
+                                }
+                            )
+                        }
                     }
-                )
+                }
             }
+            else if(this.tipo=="ap"){
+                if(this.entrada.length>3){
+                    let valido=true;
+                    for(var dato of this.datos){
+                        valido=true;
+                        var comp = dato.nombre.split('-')[0].substr(0,this.entrada.length);
+                        if(comp!=this.entrada) valido=false;
+                        if(valido){
+                            this.validos.push(
+                                {
+                                "id": this.validos.length + 1,
+                                "codigo": dato.codigo,
+                                "especialidad": dato.especialidad,
+                                "ap": dato.nombre.split('-')[0],
+                                "am": dato.nombre.split('-')[1],
+                                "nombre": dato.nombre.split('-')[2]
+                                }
+                            )
+                        }
+                    }
+                }
             }
-        }
-        this.codigo=""
+            else if(this.tipo=="am"){
+                if(this.entrada.length>3){
+                    let valido=true;
+                    for(var dato of this.datos){
+                        valido=true;
+                        var comp = dato.nombre.split('-')[1].substr(0,this.entrada.length);
+                        if(comp!=this.entrada) valido=false;
+                        if(valido){
+                            this.validos.push(
+                                {
+                                "id": this.validos.length + 1,
+                                "codigo": dato.codigo,
+                                "especialidad": dato.especialidad,
+                                "ap": dato.nombre.split('-')[0],
+                                "am": dato.nombre.split('-')[1],
+                                "nombre": dato.nombre.split('-')[2]
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+            else {
+                if(this.entrada.length>3){
+                    let valido=true;
+                    for(var dato of this.datos){
+                        valido=true;
+                        if(typeof(dato.nombre.split('-')[2])=="string"){
+                            var comp = dato.nombre.split('-')[2].substr(0,this.entrada.length);
+                            if(comp!=this.entrada) valido=false;
+                            if(valido){
+                                this.validos.push(
+                                    {
+                                    "id": this.validos.length + 1,
+                                    "codigo": dato.codigo,
+                                    "especialidad": dato.especialidad,
+                                    "ap": dato.nombre.split('-')[0],
+                                    "am": dato.nombre.split('-')[1],
+                                    "nombre": dato.nombre.split('-')[2]
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        this.entrada="";
         }
     },
 }
@@ -91,9 +164,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 20px;
+  background: #000;
+  min-height: 100vh;
 }
-.center{
-  margin: auto;
+.table{
+    width: 90%;
 }
 </style>
