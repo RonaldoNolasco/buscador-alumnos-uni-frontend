7a5b77529700ca2b9d34c7dd2f6ccbbfe910315f
TheBestShooter <rnolascoc@uni.pe>
2020-02-12 00:33:07 -0500

    Ahora si

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ const port = process.env.PORT || 8080
 app.listen(port)
 
 console.log('Listening on port ' + port)
+//npm run build
+//npm install express serve-static --save
+//poner el server.js y crear el comandos en el package.json
+//quitar el /dist del gitignore
+//heroku login
 //heroku git:remote -a buscador-alumnos-uni
+//add commit y push -f
