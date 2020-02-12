da99c93ee1d3830963d35f4d67c0da59dd5a0aef
TheBestShooter <rnolascoc@uni.pe>
2020-02-12 00:22:55 -0500

    final v2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import './../node_modules/bulma/css/bulma.css';
+
 
 Vue.config.productionTip = false
 
