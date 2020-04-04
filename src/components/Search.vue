ebe1f7a65ba273cda91aea9696e1d11484009a2f
TheBestShooter <rnolascoc@uni.pe>
2020-04-04 00:28:59 -0500

    con api

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,13 +1,10 @@
 <template>
     <div>
         <div class="p-3 bg-dark text-white">
-            <input class="col-lg-8 col-md-6 col-sm-6 col-11 form-control mr-sm-2 mr-2 mb-2" type="text" :placeholder="'Ingrese el '+relacion(tipo)" v-model="entrada" @keyup.enter="buscar" oninput="this.value = this.value.toUpperCase()">
-            <select class="col-lg-2 col-md-3 col-sm-3 col-7 form-control mr-sm-2 mr-2" v-model="tipo">    
-                <option value="codigo">Código</option>
-                <option value="ap">Apellido Paterno</option>
-                <option value="am">Apellido Materno</option>
-                <option value="nombre">Nombre</option>
-            </select>
+            <input class="col-lg-2 col-md-2 col-sm-2 col-2 form-control mr-sm-2 mr-2 mb-2" type="text" :placeholder="'Código'" v-model="codigo" oninput="this.value = this.value.toUpperCase()">
+            <input class="col-lg-2 col-md-2 col-sm-2 col-2 form-control mr-sm-2 mr-2 mb-2" type="text" :placeholder="'Apellido paterno'" v-model="ap" oninput="this.value = this.value.toUpperCase()">
+            <input class="col-lg-2 col-md-2 col-sm-2 col-2 form-control mr-sm-2 mr-2 mb-2" type="text" :placeholder="'Apellido materno'" v-model="am" oninput="this.value = this.value.toUpperCase()">
+            <input class="col-lg-2 col-md-2 col-sm-2 col-2 form-control mr-sm-2 mr-2 mb-2" type="text" :placeholder="'Nombre'" v-model="nombre" oninput="this.value = this.value.toUpperCase()">
             <button class="col-lg-1 col-md-2 col-sm-2 col-3 form-control btn btn-outline-success" @click="buscar">Buscar</button>
         </div>
         <Table :validos="validos"/>
@@ -23,64 +20,34 @@ export default {
     },
     data() {
         return {
-            entrada:'',
-            tipo:'codigo',
-            datos:require('./../../public/codigos.json'),
-            arrayRelacion:[
-                { key:'codigo',value:'Código' },
-                { key:'ap', value:'Apellido Paterno' },
-                { key:'am', value:'Apellido Materno' },
-                { key:'nombre', value:'Nombre' },
-            ],
+            codigo:"",
+            ap:"",
+            am:"",
+            nombre:"",
             validos:[],
         }
     },
     methods:{
         buscar(){
-            this.validos=[]
-            let valInput=false
-            if(this.tipo=="codigo" && this.entrada.length>5) valInput=true;
-            if(this.tipo=="ap" && this.entrada.length>3) valInput=true;
-            if(this.tipo=="am" && this.entrada.length>3) valInput=true;
-            if(this.tipo=="nombre" && this.entrada.length>3) valInput=true;
-            if(valInput){
-                let valido=true
-                for (var dato of this.datos){
-                    valido=true
-                    if(this.tipo=="codigo") var comp = dato.codigo.substr(0,this.entrada.length);
-                    if(this.tipo=="ap") var comp = dato.nombre.split('-')[0].substr(0,this.entrada.length);
-                    if(this.tipo=="am") var comp = dato.nombre.split('-')[1].substr(0,this.entrada.length);
-                    if(typeof(dato.nombre.split('-')[2])=="string"){
-                        if(this.tipo=="nombre") var comp = dato.nombre.split('-')[2].substr(0,this.entrada.length);
-                    }
-                    if(comp!=this.entrada) valido=false;
-                    if(valido){
-                        this.validos.push(
-                            {
-                            "id": this.validos.length + 1,
-                            "imgUrl": "https://www.orce.uni.edu.pe/fotosuni/0060"+dato.codigo+".jpg",
-                            "codigo": dato.codigo,
-                            "especialidad": dato.especialidad,
-                            "ap": dato.nombre.split('-')[0],
-                            "am": dato.nombre.split('-')[1],
-                            "nombre": dato.nombre.split('-')[2]
-                            }
-                        )
+            let response = fetch(
+                'http://localhost:8081/api/registrox?'+'codigo='+this.codigo+'&nombre='+this.nombre+'&ap='+this.ap+'&am='+this.am,
+                {
+                    method:'GET',
+                    headers:{
+                        'Content-Type':'application/json;charset=utf-8'
                     }
                 }
-            }
-            this.entrada="";
-        },
-        relacion(cad){
-            for (var rela of this.arrayRelacion){
-                if (rela.key==cad) {
-                    return rela.value;
-                    break;
+            )
+            .then(response => response.json())
+            .then(data => 
+                {
+                    this.validos = data
+                    for(let [index,value] of this.validos.entries()){
+                        this.validos[index].id = index+1
+                        this.validos[index].imgUrl = "https://www.orce.uni.edu.pe/fotosuni/0060"+this.validos[index].codigo+".jpg"
+                    }
                 }
-            }
-        },
-        agregar(){
-            this.validos.push(5);
+            );
         }
     }
 }
